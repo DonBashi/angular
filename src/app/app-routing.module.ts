70b2776684065505c9918e59b06c85319d4c2d8b
Bashi <S_Bowerman925@yahoo.com>
2018-06-30 20:39:36 -0700

    Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { AboutComponent } from './about/about.component';
+import { ApiComponent } from './api/api.component';
+import { CryptographyComponent } from './cryptography/cryptography.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:'', component:LandingComponent},
   {path:'about', component:AboutComponent}
+  {path:'api', component:ApiComponent},
+  {path:'cryptography', component:CryptographyComponent},
+  {path:'contact', component:ContactComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ApiComponent } from './api/api.component';
+import { CryptographyComponent } from './cryptography/cryptography.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    ApiComponent,
+    CryptographyComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
